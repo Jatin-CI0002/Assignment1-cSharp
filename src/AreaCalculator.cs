4428a9f047f6c98db7980b3d7a946960349a22d1
Jatin-CI0002 <jatin@caizin.com>
2022-10-04 16:49:16 +0530

    updated after review

---
+++ src/AreaCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignments
+{
+    public class AreaCalculator
+    {
+        public static double CalculateRequiredArea(int input)
+        {
+            double radius = 0;
+            if (input != 0)
+                radius = Convert.ToDouble(input);
+
+            else
+                return 0;
+
+            double rSquare = radius * radius;
+            double triangle = rSquare;  
+            double rightWridge = rSquare - ((0.25 * Math.PI * rSquare));
+            double sectorArea = ((rSquare * (Math.PI - 2 * Math.Atan(0.5)) / 2) - (rSquare * 0.4));
+
+            return Math.Round(triangle - (rightWridge + sectorArea),3);
+        }
+    }
+}
---
+++ src/Centrifuge.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace Assignments
+{
+    public class Centrifuge
+    {
+        public static bool BalanceTheConfiguration(int total, int filled)
+        {
+            bool isSum = (isSumOfFactors(total, filled));
+            return (total > 1 && (total == filled || isSum));
+        }
+
+        private static List<int> FindPrimeFactors(int n)
+        {
+            List<int> primeFactors = new List<int>();
+
+            bool[] prime = new bool[n + 1];
+
+            for (int i = 0; i <= n; i++)
+                prime[i] = true;
+
+            for (int p = 2; p * p <= n; p++)
+                if (prime[p] == true)
+                    for (int i = p * p; i <= n; i += p)
+                        prime[i] = false;
+
+            // Print all prime numbers
+            for (int i = 2; i <= n; i++)
+                if (prime[i] == true)
+                    primeFactors.Add(i);
+
+            return primeFactors;
+        }
+
+        private static bool isSumOfFactors(int total, int filled) {
+            List<int> primeFactors = FindPrimeFactors(total);
+
+            int sum = 0;
+            foreach (int factor in primeFactors)
+            {
+                sum += factor;
+                if (sum == filled || sum == total - filled)
+                    return true;
+                else
+                    continue;
+            }
+            return false;
+        }
+    }
+    }
+
